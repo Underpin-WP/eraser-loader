b7348be94b17378824453f3f09022add1b739241
Alex Standiford <a@alexstandiford.com>
2021-05-03 16:27:17 -0700

    Fixes issues where extended plugins did not get registered items.

--- erasers.php
+++ erasers.php
@@ -4,13 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $class ) {
-	if ( 'Underpin\Underpin' === $class ) {
+add_action( 'underpin/before_setup', function ( $instance ) {
 		require_once( plugin_dir_path( __FILE__ ) . 'Eraser.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'Eraser_Instance.php' );
-		Underpin\underpin()->loaders()->add( 'erasers', [
+		$instance->loaders()->add( 'erasers', [
 			'instance' => 'Underpin_Erasers\Abstracts\Eraser',
 			'default'  => 'Underpin_Erasers\Factories\Eraser_Instance',
 		] );
-	}
 } );
