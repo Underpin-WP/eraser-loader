f9a59387a6d01723fd3dbc5830489798785a6d90
Alex Standiford <a@alexstandiford.com>
2021-05-05 15:51:06 -0700

    Corrects underpin autoloader

--- erasers.php
+++ erasers.php
@@ -4,10 +4,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $instance ) {
+add_action( 'underpin/before_setup', function ( $file ) {
 		require_once( plugin_dir_path( __FILE__ ) . 'Eraser.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'Eraser_Instance.php' );
-		$instance->loaders()->add( 'erasers', [
+		Underpin\underpin()->get( $file )->loaders()->add( 'erasers', [
 			'instance' => 'Underpin_Erasers\Abstracts\Eraser',
 			'default'  => 'Underpin_Erasers\Factories\Eraser_Instance',
 		] );
