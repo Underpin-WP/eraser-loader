f476d9ba5b3eb6ec6a105a08b8ea09f6316cbbcd
Alex Standiford <a@alexstandiford.com>
2021-11-21 19:04:26 -0600

    Compat for Underpin 2.0.0

--- erasers.php
+++ erasers.php
@@ -1,14 +1,19 @@
 <?php
+
+use Underpin\Abstracts\Underpin;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file, $class ) {
+Underpin::attach( 'setup', new \Underpin\Factories\Observer( 'erasers', [
+	'update' => function ( Underpin $plugin ) {
 		require_once( plugin_dir_path( __FILE__ ) . 'Eraser.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'Eraser_Instance.php' );
-		Underpin\underpin()->get( $file, $class )->loaders()->add( 'erasers', [
+		$plugin->loaders()->add( 'erasers', [
 			'instance' => 'Underpin_Erasers\Abstracts\Eraser',
 			'default'  => 'Underpin_Erasers\Factories\Eraser_Instance',
 		] );
-}, 10, 2 );
+	},
+] ) );
